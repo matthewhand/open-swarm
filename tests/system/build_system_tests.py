8a368ae53e52cb87a0530fe7ac3ed78a6321afba
Matthew Hand <matthewhandau@gmail.com>
2025-03-14 09:56:41 +1100

    test: add build script for generating system tests for blueprints

---
+++ tests/system/build_system_tests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+
+def main():
+    blueprints_dir = 'blueprints'
+    system_tests_dir = os.path.join('tests', 'system')
+    os.makedirs(system_tests_dir, exist_ok=True)
+
+    for d in os.listdir(blueprints_dir):
+        subdir = os.path.join(blueprints_dir, d)
+        if os.path.isdir(subdir):
+            pattern = os.path.join(subdir, 'blueprint_*.py')
+            files = glob.glob(pattern)
+            if files:
+                blueprint_file = os.path.basename(files[0])
+                command = f'echo -e "what is your purpose\\n/quit" | python {os.path.join(blueprints_dir, d, blueprint_file)}'
+                test_file_path = os.path.join(system_tests_dir, f'test_{d}.sh')
+                with open(test_file_path, 'w') as f:
+                    f.write("#!/bin/bash\n")
+                    f.write(command + "\n")
+                os.chmod(test_file_path, 0o755)
+                print(f"Created test: {test_file_path}")
+
+if __name__ == '__main__':
+    main()
