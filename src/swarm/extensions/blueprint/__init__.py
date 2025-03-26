13942584333673f7ff277c44bfac1042c04b4b33
Matthew Hand <matthewhandau@gmail.com>
2025-03-26 07:32:57 +0000

    chore(blueprint): refresh blueprint module initialization

--- src/swarm/extensions/blueprint/__init__.py
+++ src/swarm/extensions/blueprint/__init__.py
@@ -1,18 +1,25 @@
-from .agent_utils import initialize_agents, discover_initial_agent_assets
-from .common_utils import get_token_count
-from .spinner import Spinner
-from .message_utils import repair_message_payload, validate_message_sequence
+"""
+Blueprint extension initialization.
+"""
+from .agent_utils import initialize_agents, discover_tools_for_agent, discover_resources_for_agent
+from .django_utils import register_django_components
+from .message_utils import repair_message_payload, validate_message_sequence, truncate_preserve_pairs, truncate_strict_token, truncate_recent_only
 from .blueprint_base import BlueprintBase
-
 from .blueprint_discovery import discover_blueprints
+# Added spinner explicitly to __all__ if it's meant to be public
+from .spinner import Spinner
 
 __all__ = [
     "initialize_agents",
-    "discover_initial_agent_assets",
-    "discover_blueprints",
-    "get_agent_name",
-    "get_token_count",
-    "Spinner",
+    "discover_tools_for_agent",
+    "discover_resources_for_agent",
+    "register_django_components",
     "repair_message_payload",
     "validate_message_sequence",
+    "truncate_preserve_pairs",
+    "truncate_strict_token",
+    "truncate_recent_only",
+    "BlueprintBase",
+    "discover_blueprints",
+    "Spinner", # Added Spinner
 ]
